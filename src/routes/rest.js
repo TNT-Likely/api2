1b4c6b9b395c6f7bbb3d3194900b3d3047e4e4dc
likely <532837139@qq.com>
2016-10-13 17:41:44 +0800

    basic rest

--- src/routes/rest.js
+++ src/routes/rest.js
@@ -9,9 +9,10 @@ let handler = (promise, res) => {
 
 export let restRouter = (app) => {
   let router = express.Router()
+  let model = null
 
   router.use('/:model', (req, res, next) => {
-    let model = app.models[req.params.model]
+    model = app.models[req.params.model]
     if (!model) res.status(404).send(`model ${req.params.model} not exists!`)
     else {
       next()
@@ -19,9 +20,7 @@ export let restRouter = (app) => {
   })
 
   router.use('/:model/:id', (req, res) => {
-    let model = app.models[req.params.model]
     let id = req.params.id
-
     switch (req.method) {
       case 'PUT':
         handler(model.update({ id: id }, req.body), res)
@@ -33,7 +32,6 @@ export let restRouter = (app) => {
   })
 
   router.use('/:model', (req, res) => {
-    let model = app.models[req.params.model]
     switch (req.method) {
       case 'POST':
         handler(model.create(req.body), res)
