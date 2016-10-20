a56a8547e41ca627b792abfb20a2616a0fecc370
likely <532837139@qq.com>
2016-10-20 21:52:17 +0800

    move user logic to router not  model

--- src/models/user.js
+++ src/models/user.js
@@ -1,62 +1,38 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs'
 import { uid } from '../tools'
-import cookieModel from './cookie'
 
 let Schema = new mongoose.Schema({
-  username: { type: String, required: true, max: 20, unique: true },
-  password: { type: String, required: true },
-  //手机验证
-  phone: {
-    type: String,
-    validate: {
-      validator: function(v) {
-        return /1\d{10}/.test(v);
-      },
-      message: '{VALUE} is not a valid phone number!'
+    username: { type: String, required: true, max: 20, unique: true },
+    password: { type: String, required: true },
+    //手机验证
+    phone: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /1\d{10}/.test(v);
+            },
+            message: '{VALUE} is not a valid phone number!'
+        },
+        unique: true
     },
-    unique: true
-  },
-  //邮箱验证
-  email: { type: String, required: true, unique: true, match: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ },
-  //邮箱是否激活
-  emailVerified: { type: Boolean, default: false },
-  //激活token
-  verifyToken: String,
-  //重置密码token
-  resetToken: String,
-  avatar: String
+    //邮箱验证
+    email: { type: String, required: true, unique: true, match: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ },
+    //邮箱是否激活
+    emailVerified: { type: Boolean, default: false },
+    //激活token
+    verifyToken: String,
+    //重置密码token
+    resetToken: String,
+    avatar: String
 }, { versionKey: '', timestamps: {} })
 
 //密码存入之前要加密
 Schema.post('save', (m, next) => {
-  m.password = bcrypt.hashSync(m.password, 10)
-  next()
+    m.password = bcrypt.hashSync(m.password, 10)
+    next()
 })
-
-//登录
-Schema.statics.login = (nameOrEmail, password, next) => {
-  return new Promise((resolve, reject) => {
-    model.findOne().or([{ username: nameOrEmail }, { email: nameOrEmail }]).then((r) => {
-      if (!r) resolve({ msg: '用户不存在' });
-      if (!r.emailVerified) {
-        resolve({ msg: '邮箱未激活' })
-      } else if (bcrypt.compareSync(password, r.password)) {
-        cookieModel.create({ uid: r.id }).then(rs => {
-          r._doc.accessToken = rs.accessToken
-          resolve({ data: r._doc })
-        }).catch(er => { resolve({ msg: '创建access_token失败', err: er }) })
-      } else {
-        resolve({ msg: '密码错误' })
-      }
-    }).catch(e => {
-      reject(e)
-    })
-  })
-}
 
 Schema.plugin(require('mongoose-unique-validator'))
 
-let model = mongoose.model('user', Schema)
-
-export default model
+export default mongoose.model('user', Schema)
--- src/routes/rest/user.js
+++ src/routes/rest/user.js
@@ -1,30 +1,44 @@
 let model = require('../../models')['user']
+let cookieModel = require('../../models')['cookie']
+import bcrypt from 'bcryptjs'
 import { handler, uid, emailsender } from '../../tools'
 
 export default (r) => {
-  r.post('/user/register', (req, res) => {
-    let body = req.body
-    model.create(body).then(result => {
-      result.verifyToken = uid()
-      result.save().then(r => {
-        emailsender.verify(r.email, r.verifyToken).then(result => {
-          res.json(r)
-        }).catch((err) => {
-          res.json(err)
+    r.post('/user/register', (req, res) => {
+        let body = req.body
+        model.create(body).then(result => {
+            result.verifyToken = uid()
+            result.save().then(r => {
+                emailsender.verify(r.email, r.verifyToken).then(result => {
+                    res.json({ code: 0, msg: null, data: r })
+                }).catch((err) => {
+                    res.json({ code: 4000, msg: err, data: null })
+                })
+            }).catch(err => {
+                res.json({ code: 4001, msg: err, data: null })
+            })
+        }).catch(err => {
+            res.json({ code: 4002, msg: err, data: null })
         })
-      }).catch(err => {
-        res.json(err)
-      })
-    }).catch(err => {
-      res.json(err)
     })
-  })
 
-  r.post('/user/login', (req, res) => {
-    model.login(req.body.nameOrEmail, req.body.password).then(r => {
-      res.json(r)
-    }).catch(e => {
-      res.json(e)
+    r.post('/user/login', (req, res) => {
+        let nameOrEmail = req.body.nameOrEmail
+        let password = req.body.password
+        model.findOne().or([{ username: nameOrEmail }, { email: nameOrEmail }]).then((r) => {
+            if (!r) res.json({ code: 4003, msg: '用户不存在', data: null })
+            if (!r.emailVerified) {
+                res.json({ code: 4004, msg: '邮箱未激活', data: null })
+            } else if (bcrypt.compareSync(password, r.password)) {
+                cookieModel.create({ uid: r.id }).then(rs => {
+                    r._doc.accessToken = rs.accessToken
+                    res.json({ code: 0, msg: null, data: r._doc })
+                }).catch(er => { res.json({ code: 4005, msg: er, data: null }) })
+            } else {
+                res.json({ code: 4006, msg: '密码错误', data: null })
+            }
+        }).catch(e => {
+            res.json({ code: 4007, msg: e, data: null })
+        })
     })
-  })
 }
