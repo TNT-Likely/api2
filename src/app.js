46ed6486d62fd62510a82a5e88bae5be81a71910
likely <532837139@qq.com>
2016-10-22 17:18:48 +0800

    dev use logger and catch 404

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import color from 'colors'
 import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 import mongoose from 'mongoose'
 import config from './config'
 import models from './models'
@@ -11,8 +12,8 @@ import { restRouter } from './routes'
 mongoose.connect(config.mongo.url)
 mongoose.Promise = global.Promise
 mongoose.connection.on('error', function(err) {
-  console.error('MongoDB connection error: ' + err)
-  process.exit(-1);
+    console.error('MongoDB connection error: ' + err)
+    process.exit(-1);
 })
 
 const app = express()
@@ -23,15 +24,30 @@ app.use(bodyParser())
 //parse cookie
 app.use(cookieParser())
 
+//logger middleware
+if (process.env.NODE_ENV != 'production') {
+    app.use(morgan('dev'))
+}
+
 //server static file
 app.use('/static', express.static(config.staticFolder))
 
 //rest router
 app.use(`/${config.restEndpoint}`, restRouter())
 
+//catch 404
+app.use((req, res, next) => {
+    next('not found!')
+});
+
+//error handler
+app.use((err, req, res, next) => {
+    res.status(404).send(err);
+})
+
 //start server
 app.listen(config.port, () => {
-  console.log(`start server at port ${config.port} success`.blue)
+    console.log(`start server at port ${config.port} success`.blue)
 })
 
 
