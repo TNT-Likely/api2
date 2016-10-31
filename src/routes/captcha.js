c0226f2a1aa3b4b6ea122149861c25df882f560b
likely <532837139@qq.com>
2016-10-31 14:42:20 +0800

    captcha validate middleware

--- src/routes/captcha.js
+++ src/routes/captcha.js
@@ -1,4 +1,4 @@
-import { auth, check } from '../middleware'
+import { auth, check, captcha as m_captcha } from '../middleware'
 import { captcha, crypto, handler } from '../tools'
 import Canvas from 'canvas'
 import path from 'path'
@@ -6,27 +6,22 @@ import path from 'path'
 export default (r) => {
   r.get('/new', (req, res) => {
     let Image = Canvas.Image,
-      canvas = new Canvas(200, 80),
+      canvas = new Canvas(100, 40),
       ctx = canvas.getContext('2d'),
       text = captcha.text()
 
-
-    ctx.font = '30px Impact';
-    ctx.rotate(-.1);
-    ctx.fillText(text, 50, 50);
-    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-    ctx.beginPath();
-    ctx.stroke()
+    ctx.font = '30px Impact'
+    ctx.rotate(-.1)
+    ctx.fillText(text, 10, 35)
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)'
 
     let img = canvas.toBuffer()
     res.writeHead(200, { 'Content-Type': 'image/png', 'Content-Length': img.length, 'token': crypto.encrypt(text) })
     res.end(canvas.toBuffer())
   })
 
-  r.post('/validate', check(['captcha', 'token']), (req, res) => {
-    let is = crypto.validate(req.body.captcha, req.body.token)
-    if (is) handler(res, '验证码正确')
-    else handler(res, '验证码错误', 40607)
+  r.post('/validate', check(['captcha', 'token']), m_captcha.validate, (req, res) => {
+    handler(res, '验证码正确')
   })
 
   return r
